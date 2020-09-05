124778349c2ab25aabe32571af6387500a0b74e4
Nick Hawke <n.hawke96@gmail.com>
2020-09-05 12:28:22 -0400

    Undo 8af3141 to avoid wrestling with git

--- src/commands/commands.py
+++ src/commands/commands.py
@@ -190,7 +190,7 @@ class Commands:
             # Bot can manage all roles below its highest role. Find that role 
             # and begin listing roles from that point onward.
             for role in guild.roles[:0:-1]:  # top-down, excluding @everyone
-                if can_manage and role.name != Commands.client.user.name:
+                if can_manage:
                     roles.append(role.name)
                 elif Commands.client.user in role.members:
                     can_manage = True
