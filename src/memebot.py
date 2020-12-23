20827e5874c8950dcee1af1145d3616d07a89f89
Robert Leslie <Robert.Leslie1997@gmail.com>
2020-12-22 20:59:25 -0500

    Fixed how finding a twitter URL is done, fixed and compiled twitter URL regex. Added little comment for quoted tweets

--- src/memebot.py
+++ src/memebot.py
@@ -1,6 +1,5 @@
 import json
 import re
-import shlex
 from typing import Optional
 
 import discord
@@ -32,6 +31,8 @@ class MemeBot(discord.Client):
                 twitter_tokens['access_token_secret'],
                 tweet_mode='extended'
             )
+        
+        self.twitter_url_pattern = re.compile(r'https:\/{2}twitter\.com\/([0-9a-zA-Z_]+|i\/web)\/status\/[0-9]+(\?s=\d+)?')
 
     async def on_ready(self) -> None:
         """
@@ -53,34 +54,38 @@ class MemeBot(discord.Client):
         else:
             await commands.execution.execute_if_command(message)
 
-        # Iterate through each word in the message, and checks each one if
-        # the "word" in the message is a twitter URL
-        for content in message.content.split():
-            if self.is_twitter_url(content):
-                twitter_url = content
+        # Iterate through the message sent, and check if the message conatined
+        # a "word" that was a twitter URL to a status.
+        twitter_url = self.get_twitter_url(message.content)
+        if twitter_url:
 
-                # Because a twitter URL to a status is oftentimes as follows:
-                # https://twitter.com/USER/status/xxxxxxxxxxxxxxxxxxx?s=yy
-                # We need to split up the URL by the "/", and then split it up
-                # again by the "?" in order to get the ID of the tweet being
-                # linked
-                tweet_id = twitter_url.split("/")[-1].split("?")[0]
-                tweet_info = self.twitter_api.GetStatus(tweet_id)
+            """Because a twitter URL to a status is oftentimes as follows:
+            https://twitter.com/USER/status/xxxxxxxxxxxxxxxxxxx?s=yy
+            We need to split up the URL by the "/", and then split it up
+            again by the "?" in order to get the ID of the tweet being
+            linked"""
+            tweet_id = twitter_url.split("/")[-1].split("?")[0]
+            tweet_info = self.twitter_api.GetStatus(tweet_id)
 
-                if tweet_info.media and len(tweet_info.media) > 1:
-                    emoji = ":" + str(len(tweet_info.media)) + ":"
-                    await message.add_reaction(constants.EMOJI_MAP[emoji])
+            if tweet_info.media and len(tweet_info.media) > 1:
+                emoji = ":" + str(len(tweet_info.media)) + ":"
+                await message.add_reaction(constants.EMOJI_MAP[emoji])
 
-                if tweet_info.quoted_status:
-                    quote_tweet_urls = self.get_tweet_urls(tweet_info)
-                    if quote_tweet_urls != "":
-                        await message.channel.send(quote_tweet_urls)
+            if tweet_info.quoted_status:
+                quote_tweet_urls = self.get_tweet_urls(tweet_info)
+                if quote_tweet_urls != "":
+                    await message.channel.send(quote_tweet_urls)
 
-    def is_twitter_url(self, msg: str) -> bool:
-        """Returns true if msg is a twitter link, otherwise returns False"""
-        # re.match looks for a match in msg. Regex Matches if first part of msg
-        # is https://twitter.com/XXXXXXXXXXXXXX
-        return bool(re.match(r'https:\/\/twitter\.com\b[-a-zA-Z0-9()@:%_\+.~#?&\/\/=]*', msg))
+    def get_twitter_url(self, content: str) -> str:
+        """
+        Loops through each "word" in conetnt, compares the word to a regex
+        and returns the "word" in the message that matches the twitter URL regex
+        :param content: message that was sent in discord
+        """
+        for word in content.split():
+            if self.twitter_url_pattern.match(word):
+                return word
+        return ""
 
     def get_tweet_urls(self, tweet_info: twitter.models.Status) -> str:
         """
@@ -88,7 +93,7 @@ class MemeBot(discord.Client):
         :param tweet_info: information of tweet
         :return: URL of media/quote tweet(s)
         """
-        tweets = ""
+        tweets = "quoted tweet(s): "
         for level in range(3):
             tweets += "\n https://twitter.com/i/web/status/" + tweet_info.quoted_status.id_str
             tweet_info = self.twitter_api.GetStatus(tweet_info.quoted_status.id)
