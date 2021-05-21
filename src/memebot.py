4bf84236e334e388e37152f35903772a9b7f6f66
Adam Cooper <adamcooperrc@gmail.com>
2021-05-21 07:07:16 -0400

    Move Twitter integration to separate module (#78)
    All of MemeBot's Twitter integration has been moved to a [separate module](src/integrations/twitter.py), and have been made asynchronous and stateless. The only state managed by the Twitter module is whether or not MemeBot has authenticated to the Twitter API. This is done by MemeBot using the `twitter.init` function. If MemeBot attempts to make any API calls before `init` is called, the functions which make those calls will throw.
    
    Fixes #64.

--- src/memebot.py
+++ src/memebot.py
@@ -1,13 +1,11 @@
-import json
-import re
+import asyncio
 
 import discord
-import tweepy
 
 import commands
 import config
 import db
-from lib import constants
+from integrations import twitter
 
 
 class MemeBot(discord.Client):
@@ -17,16 +15,8 @@ class MemeBot(discord.Client):
 
     def __init__(self, **args):
         super().__init__(**args, intents=discord.Intents().all())
-
-        with open(config.twitter_api_tokens) as twitter_api_tokens:
-            twitter_tokens = json.load(twitter_api_tokens)
-
-        self.twitter_api = tweepy.API(
-            tweepy.AppAuthHandler(twitter_tokens['consumer_key'], twitter_tokens['consumer_secret']))
-
-        self.twitter_url_pattern = re.compile(
-            r'(https://twitter\.com/([0-9a-zA-Z_]+|i/web)/status/[0-9]+(\?s=\d+)?)')
-
+        if config.twitter_enabled:
+            twitter.init(config.twitter_api_tokens)
         db.test()
 
     async def on_ready(self) -> None:
@@ -47,52 +37,8 @@ class MemeBot(discord.Client):
         if message.author != self.user:
             await commands.execution.execute_if_command(message)
 
-        # Iterate through the message sent, and check if the message conatined
-        # a "word" that was a twitter URL to a status.
-        twitter_url = self.get_twitter_url_from_message(message.content)
-        if twitter_url:
-            """Because a twitter URL to a status is oftentimes as follows:
-            https://twitter.com/USER/status/xxxxxxxxxxxxxxxxxxx?s=yy
-            We need to split up the URL by the "/", and then split it up
-            again by the "?" in order to get the ID of the tweet being
-            linked"""
-            tweet_id = twitter_url.split("/")[-1].split("?")[0]
-            tweet_info = self.twitter_api.get_status(tweet_id)
-            tweet_media = tweet_info.extended_entities['media'] if 'media' in tweet_info.entities else []
+        if config.twitter_enabled:
+            asyncio.create_task(twitter.process_message_for_interaction(message))
 
-            if len(tweet_media) > 1:
-                emoji = ":" + str(len(tweet_media)) + ":"
-                await message.add_reaction(constants.EMOJI_MAP[emoji])
-
-            if tweet_info.is_quote_status:
-                quote_tweet_urls = self.get_quote_tweet_urls(tweet_info)
-                await message.channel.send(quote_tweet_urls)
-
-    def get_twitter_url_from_message(self, content: str) -> str:
-        """
-        Loops through each "word" in content, compares the word to a regex
-        and returns the "word" in the message that matches the twitter URL regex
-        :param content: message that was sent in discord
-        """
-        match = self.twitter_url_pattern.search(content)
-        if match:
-            return match.groups()[0]
-        else:
-            return ""
-
-    def get_quote_tweet_urls(self, tweet_info: tweepy.models.Status) -> str:
-        """
-        Gets URLs of quoted tweets (nested up to max 3)
-        :param tweet_info: information of tweet
-        :return: URL of media/quote tweet(s)
-        """
-        tweets = "quoted tweet(s): "
-        for _ in range(3):
-            tweets += "\nhttps://twitter.com/" + tweet_info.quoted_status.author.screen_name + "/status/" + tweet_info.quoted_status.id_str
-            tweet_info = self.twitter_api.get_status(tweet_info.quoted_status.id)
-            if not tweet_info.is_quote_status:
-                break
-        return tweets
-
-
+        
 client: discord.Client = MemeBot()
