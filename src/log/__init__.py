6e289d347ba07b023327f89f8d31a51a52d96d33
Adam Cooper <adam@adamcooper.sh>
2021-06-12 02:30:13 -0400

    Remove `log.log_all`
    Our logging system has been outfitted with a custom handler that now performs the functionality of log_all automagically when needed. The [`MemeBotLogHandler`](src/log/handler.py) assumes the behavior of whatever native handler is chosen by the config package, but injects some code that splits multi-line log messages into multiple emits as opposed to one. This way, every line of a long log message will be properly aligned 😤

--- src/log/__init__.py
+++ src/log/__init__.py
@@ -1,7 +1,6 @@
 import atexit
 import contextlib
 import logging
-from typing import Callable, Iterable
 
 import config
 from . import formatter, logger
@@ -30,18 +29,6 @@ def set_third_party_logging():
     if config.log_level == logging.getLevelName(logging.DEBUG):
         asyncio.get_event_loop().set_debug(True)
         tweepy.debug()
-
-
-def log_all(logging_func: Callable, messages: Iterable[str], *args, **kwargs):
-    """
-    Create a multiple log statements for a collection of messages.
-    :param logging_func: The function to use for logging i.e. self.debug, self.info, etc.
-    :param messages: The list of messages to log
-    :param args: Additional args; passed straight to logging_func
-    :param kwargs: Additional kwargs; passed straight to logging_func
-    """
-    for line in messages:
-        logging_func(line, *args, **kwargs)
 
 
 # Forward memebot_logger's logging methods as module-level functions
