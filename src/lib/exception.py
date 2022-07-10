db68fc2332c6fc34d02857508975a72be8a8714c
Adam Cooper <adam@adamcooper.sh>
2022-07-09 19:00:04 -0500

    Fix comment

--- src/lib/exception.py
+++ src/lib/exception.py
@@ -6,7 +6,8 @@ class MemebotUserError(discord.ext.commands.CommandError):
     Exception which carries a message to be displayed to the user
     """
 
+
 class MemebotInternalError(discord.ext.commands.CommandError):
     """
-    Exception which
+    Exception which is thrown on purpose, but whose content would likely confuse a user.
     """
