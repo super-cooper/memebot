fd23fed7597a992dab9ca311606a3e62ec8e4aa0
Nick Hawke <n.hawke96@gmail.com>
2022-08-09 22:45:08 -0400

    Add spoiler tags to twitter links (#115)
    If the user sends a message with a twitter link, memebot will respond with links to quote tweets or video links if necessary. However, if the user send the twitter link within a spoiler tag, memebot sends its usual replies, but those replies are un-spoiled.
    
    This change adds spoiler tags to memebot's replies if the user's twitter link is within a spoiled part of the message.
    
    fixes #114

---
+++ src/lib/util.py
@@ -0,0 +1,45 @@
+from typing import Tuple, List
+
+
+def is_spoil(message: str, idx: int) -> bool:
+    """Returns whether idx of message is within a spoiler text"""
+    if idx >= len(message) or idx < 0:
+        return False
+
+    # list of tuples of spoil ranges, [start, stop)
+    # e.g.: blah blah ||bababababababababab||
+    #                   ^start             ^end
+    spoil_ranges: List[Tuple[int, int]] = []
+    in_spoil = False
+    spoil_start = -1
+    i = 0
+    while i < len(message):
+        c = message[i]
+        if c == "\\" and not in_spoil:
+            # ignore backslashes only for the start of the spoiler
+            i += 1
+        elif c == "|":
+            if i + 1 < len(message) and message[i + 1] == "|":
+                if in_spoil:
+                    # reached the end of a spoiled section
+                    spoil_ranges.append((spoil_start, i))
+                else:
+                    # start of spoil section
+                    spoil_start = i + 2
+
+                i += 1
+                in_spoil = not in_spoil
+
+        i += 1
+
+    for start, end in spoil_ranges:
+        if start <= idx < end:
+            return True
+    return False
+
+
+def maybe_make_link_spoiler(message: str, spoil: bool) -> str:
+    """Returns message maybe wrapped in spoiler tags."""
+    # Discord will only embed links with spoilers if there is padding
+    # between the spoiler tags and the link.
+    return f"|| {message} ||" if spoil else message
