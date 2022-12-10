2379057bc2d430e438ecbe998ff9abc74c726ea5
Nick Hawke <n.hawke96@gmail.com>
2022-12-09 21:30:53 -0500

    Convert commands to slash commands

--- src/commands/__init__.py
+++ src/commands/__init__.py
@@ -1,4 +1,3 @@
 from .hello import hello
-from .help import Help
 from .poll import poll
 from .role import role
--- src/memebot.py
+++ src/memebot.py
@@ -16,6 +16,8 @@ async def on_ready() -> None:
     if not memebot.user:
         raise exception.MemebotInternalError("Memebot is not logged in to Discord")
     log.info(f"Logged in as {memebot.user}")
+    synced = await memebot.tree.sync()
+    log.info(f"Synced {len(synced)} command(s)")
     if config.twitter_enabled:
         twitter.init(memebot.user)
     if config.database_enabled:
@@ -27,41 +29,31 @@ async def on_ready() -> None:
 
 
 async def on_command_error(
-    ctx: discord.ext.commands.Context, error: discord.ext.commands.CommandError
+    interaction: discord.Interaction, error: discord.app_commands.AppCommandError
 ) -> None:
-    invocation = " ".join(ctx.message.content.split())
-    if isinstance(error, exception.MemebotInternalError):
-        # For intentionally thrown internal errors
-        log.exception(f"`{invocation}` raised an internal exception: ", exc_info=error)
-        await ctx.send(f"Internal error occurred with command `{invocation}`")
-    elif isinstance(error, discord.ext.commands.CommandInvokeError):
-        # For uncaught exceptions
-        # (discord.py wraps these in a CommandInvokeError and re-raises)
-        log.exception(
-            f"`{invocation}` raised an unhandled exception: ", exc_info=error.original
-        )
-        await ctx.send(f"Internal error occurred with command `{invocation}`")
-    else:
-        # For user-facing errors
-        # This is the easiest way to attach the exception to the context such that
-        # the help command is able to access it
-        ctx.kwargs["error"] = error
-        await ctx.send_help(ctx.command)
+    command = interaction.command
+    if not isinstance(command, discord.app_commands.Command):
+        log.warning("Interaction not a command")
+        return
+    log.exception(f"`{command.name}` raised an unhandled exception: ", exc_info=error)
+    await interaction.response.send_message(
+        error,
+        ephemeral=True,
+    )
 
 
 memebot = discord.ext.commands.Bot(
     command_prefix="!",
-    help_command=commands.Help(),
     intents=discord.Intents().all(),
-    activity=discord.Game(name="• !help"),
+    activity=discord.Game(name="• /hello"),
 )
 
-memebot.add_command(commands.hello)
-memebot.add_command(commands.poll)
-memebot.add_command(commands.role)
+memebot.tree.add_command(commands.hello)
+memebot.tree.add_command(commands.poll)
+memebot.tree.add_command(commands.role)
 
 memebot.add_listener(on_ready)
-memebot.add_listener(on_command_error)
+memebot.tree.error(on_command_error)
 if config.twitter_enabled:
     memebot.add_listener(twitter.process_message_for_interaction, "on_message")
 
