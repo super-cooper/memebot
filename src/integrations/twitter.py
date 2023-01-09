f96b32b5478abaa568ba2550c3763361ee045233
Adam Cooper <adam@adamcooper.sh>
2023-01-09 01:55:17 -0600

    Switch Twitter integration to V2 API
    This change migrates all of our old Twitter integration to the V2 API via [Tweepy](https://github.com/tweepy/tweepy).
    
    In order to use the V2 API, we have to migrate with OAuth 2.0 instead of OAuth 1.0. Because of this, the config variables for the Twitter API consumer keys have been removed, and the command-line parameters and environment variables for them have been deprecated. Twitter integration now only accepts a bearer token.
    
    Besides that, all behavior with provided by Twitter integration should remain unchanged to the user. The code itself has become slightly more complex, as Tweepy is not as pythonic an API wrapper as [discord.py](https://github.com/Rapptz/discord.py) and thus does not provide much help to simplify the more-complex V2 API.
    
    I was able to figure out the issues with migrating #97 to V2 which were discovered in the initial investigation of V2 during #67. This is the reason why references to V1 are fully removed. It seems that all behavior of V1 is replicable with V2, albeit not always as pretty.
    
    This resolves #107.

--- src/integrations/twitter.py
+++ src/integrations/twitter.py
@@ -21,7 +21,7 @@ twitter_url_pattern = re.compile(
 )
 
 # Twitter API handle
-twitter_api: tweepy.API
+twitter_api: tweepy.Client
 
 # Current bot user
 bot_user: discord.ClientUser
@@ -36,21 +36,17 @@ def init(user: discord.ClientUser) -> None:
     """
     global twitter_api
     global bot_user
-    twitter_api = tweepy.API(
-        tweepy.AppAuthHandler(
-            config.twitter_api_consumer_key, config.twitter_api_consumer_secret
-        )
-    )
+    twitter_api = tweepy.Client(bearer_token=config.twitter_api_bearer_token)
     bot_user = user
 
 
-def get_twitter_api() -> tweepy.API:
+def get_twitter_api() -> tweepy.Client:
     """
     Utility function for acquiring the module's API object, as to skip the null-checking
     :raises ValueError: If init() has not yet been called, making _twitter_api None
     :return: _twitter_api, if it has been initialized.
     """
-    if twitter_api is None:
+    if not twitter_api:
         raise ValueError("Twitter integration module has not yet been initialized.")
     return twitter_api
 
@@ -68,24 +64,60 @@ def get_twitter_url_from_message_content(content: str) -> Tuple[str, bool]:
         return "", False
 
 
-def get_quote_tweet_urls(tweet_info: tweepy.models.Status, spoiled: bool) -> str:
+def fetch_tweet_with_expansions(
+    tweet_id: str,
+) -> tuple[tweepy.Tweet, list[tweepy.User], list[tweepy.Media]]:
+    """
+    Fetches a Tweet from the Twitter API, along with relevant user and media detail
+    """
+    # Fetch the Tweet and requested expansions
+    response = get_twitter_api().get_tweet(
+        tweet_id,
+        media_fields=["url", "variants"],
+        user_fields=["username"],
+        expansions=["attachments.media_keys", "referenced_tweets.id.author_id"],
+    )
+
+    # Verify API response
+    if not isinstance(response, tweepy.Response):
+        raise TypeError(f"Unexpected Twitter API response type: ({type(response)})")
+    tweet_info = response.data
+    if not isinstance(tweet_info, tweepy.Tweet):
+        raise TypeError(f"Unexpected response data: {tweet_info}")
+    tweet_media = response.includes.get("media", [])
+    tweet_users = response.includes.get("users", [])
+
+    return tweet_info, tweet_users, tweet_media
+
+
+def is_quote_tweet(tweet: tweepy.Tweet) -> bool:
+    return tweet.referenced_tweets and any(
+        ref.type == "quoted" for ref in tweet.referenced_tweets
+    )
+
+
+def get_quote_tweet_urls(root_tweet: tweepy.Tweet, spoiled: bool) -> str:
     """
     Gets URLs of quoteted tweets (nested up to max 3)
-    :param tweet_info: information of tweet
+    :param root_tweet: information of tweet
     :param spoiled: whether the quote tweets should be spoiled
-    :return: URL of media/quote tweet(s)
+    :return: URL of quote tweet(s)
     """
-    tweets = "quoted tweet(s): "
+    output_text = "quoted tweet(s): "
+    parent_tweet = root_tweet
     for _ in range(3):
-        tweets += util.maybe_make_link_spoiler(
-            f"\nhttps://twitter.com/{tweet_info.quoted_status.author.screen_name}"
-            f"/status/{tweet_info.quoted_status.id_str}",
+        # Find the ID of the Tweet quoted by the parent
+        qt_ref = [t for t in parent_tweet.referenced_tweets if t.type == "quoted"][0]
+        qt, qt_users, _ = fetch_tweet_with_expansions(qt_ref.id)
+        qt_author = [u for u in qt_users if u.id == qt.author_id][0]
+        output_text += util.maybe_make_link_spoiler(
+            f"\nhttps://twitter.com/{qt_author.username}/status/{qt.id}",
             spoiled,
         )
-        tweet_info = get_twitter_api().get_status(tweet_info.quoted_status.id)
-        if not tweet_info.is_quote_status:
+        if not is_quote_tweet(qt):
             break
-    return tweets
+        parent_tweet = qt
+    return output_text
 
 
 async def process_message_for_interaction(message: discord.Message) -> None:
@@ -105,24 +137,21 @@ async def process_message_for_interaction(message: discord.Message) -> None:
         # We need to split up the URL by the "/", and then split it up again
         # by the "?" in order to get the ID of the tweet being linked
         tweet_id = twitter_url.split("/")[-1].split("?")[0]
-        tweet_info = get_twitter_api().get_status(tweet_id)
-        tweet_media = (
-            tweet_info.extended_entities["media"]
-            if "media" in tweet_info.entities
-            else []
-        )
+
+        tweet_info, _, tweet_media = fetch_tweet_with_expansions(tweet_id)
 
         # React with a numeric emoji to Tweets containing multiple images
         if (n_images := len(tweet_media)) > 1:
             asyncio.create_task(
                 message.add_reaction(emoji.emojize(f":keycap_{n_images}:"))
             )
-        elif n_images == 1 and "video_info" in tweet_media[0]:
+        # For tweets containing only a single video, we embed the video in Discord.
+        elif n_images == 1 and tweet_media[0].type in ("video", "animated_gif"):
             # The media_url of a media dict is actually a thumbnail
             # To get the video, we have to pull it out of its video_info
             # We will choose the variant with the highest bitrate
             video_url = max(
-                tweet_media[0]["video_info"]["variants"],
+                tweet_media[0].data["variants"],
                 key=lambda v: int(v.get("bitrate", -1)),
             )["url"]
             asyncio.create_task(
@@ -133,6 +162,6 @@ async def process_message_for_interaction(message: discord.Message) -> None:
             )
 
         # Post quote tweet links.
-        if tweet_info.is_quote_status and message.author != bot_user:
+        if is_quote_tweet(tweet_info) and message.author != bot_user:
             quote_tweet_urls = get_quote_tweet_urls(tweet_info, spoiled)
             asyncio.create_task(message.channel.send(quote_tweet_urls))
