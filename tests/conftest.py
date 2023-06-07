7e0b103e56801f84f4f53f1f23d1ffd293e50396
Adam Cooper <adam@adamcooper.sh>
2023-06-07 05:46:20 -0500

    Add initial test fixtures

---
+++ tests/conftest.py
@@ -0,0 +1,4 @@
+pytest_plugins = [
+    "pytest_asyncio",
+    "tests.fixtures.general_fixtures",
+]
---
+++ tests/fixtures/general_fixtures.py
@@ -0,0 +1,33 @@
+import os
+from unittest import mock
+
+import pytest
+
+from memebot import config
+from memebot.integrations import twitter
+
+DEFAULT_ENVIRONMENT = os.environ | {
+    "MEMEBOT_DISCORD_CLIENT_TOKEN": "MOCK_TOKEN",
+    "MEMEBOT_TWITTER_ENABLED": "False",
+}
+
+
+@pytest.fixture(autouse=True)
+def setup_and_teardown(mock_twitter_client: mock.Mock) -> None:
+    """
+    This is a universal setup and teardown function which is automatically run
+    surrounding each test. The function contains both the setup and teardown behavior.
+    The ``yield`` statement is where the test is run.
+    """
+    # Set up
+    # This creates a simple base environment. Tests that require specific configuration
+    # can overwrite the variables they need
+    os.environ = DEFAULT_ENVIRONMENT
+
+    with mock.patch("argparse.ArgumentParser", mock.MagicMock()):
+        config.populate_config_from_command_line()
+
+    # Run test
+    yield
+
+    # Tear down
