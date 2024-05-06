48aa9252436df552656562b9f663dc264024673c
Adam Cooper <adam@adamcooper.sh>
2024-05-05 20:21:52 -0400

    Use constant display name in test for `hello.py`

--- tests/commands/test_hello.py
+++ tests/commands/test_hello.py
@@ -7,8 +7,9 @@ from memebot import commands
 
 @pytest.mark.asyncio()
 async def test_hello(mock_interaction: mock.Mock) -> None:
+    mock_interaction.user.display_name = "Test Name"
     await commands.hello.callback(mock_interaction)
 
     mock_interaction.response.send_message.assert_awaited_once_with(
-        f"Hello, {mock_interaction.user.display_name}!"
+        f"Hello, Test Name!"
     )
