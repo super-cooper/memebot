290ee9a2b2b593d3c5fb8e3d1f85c3e13002a61f
Nick Hawke <n.hawke96@gmail.com>
2025-06-10 19:23:32 -0400

    Address comments

--- memebot/commands/paywall.py
+++ memebot/commands/paywall.py
@@ -1,11 +1,11 @@
-from typing import Optional, cast
+from typing import cast
 import re
 
 import discord
 
-from memebot import log
 from memebot.lib import exception
 
+URL_REGEX = r"https?://\S+"
 
 @discord.app_commands.command()
 async def paywall(interaction: discord.Interaction, link: str) -> None:
@@ -13,6 +13,11 @@ async def paywall(interaction: discord.Interaction, link: str) -> None:
     Generate the given link without a paywall. If replying to a message, use
     the link in the replied-to message
     """
+    if not re.fullmatch(URL_REGEX, link):
+        raise exception.MemebotUserError("Invalid link")
+
+    link = remove_params(link)
+
     await interaction.response.send_message(
         f"Link without paywall: https://archive.is/newest/{link}"
     )
@@ -22,7 +27,7 @@ async def paywall(interaction: discord.Interaction, link: str) -> None:
 async def paywall_context_menu(
     interaction: discord.Interaction, message: discord.Message
 ) -> None:
-    link = extract_link(message)
+    link = remove_params(extract_link(message))
     await interaction.response.send_message(
         f"Link without paywall: https://archive.is/newest/{link}"
     )
@@ -37,7 +42,11 @@ def extract_link(message: discord.Message) -> str:
 
     # This should return a list of strings:
     # https://docs.python.org/3/library/re.html#re.findall
-    for match in re.findall(r"https?://\S+", message.content):
+    for match in re.findall(URL_REGEX, message.content):
         return cast(str, match)
 
     raise exception.MemebotUserError("Cannot extract link from replied-to message")
+
+def remove_params(url: str) -> str:
+    """Remove the pesky query parameters from the given URL"""
+    return url.partition('?')[0]
--- tests/commands/test_paywall.py
+++ tests/commands/test_paywall.py
@@ -12,6 +12,11 @@ class StringContaining(str):
         return self in other
 
 
+class StringNotContaining(str):
+    def __eq__(self, other):
+        return self not in other
+
+
 @pytest.mark.asyncio
 async def test_paywall_command(mock_interaction: mock.Mock) -> None:
     link = "https://foo.com/poop"
@@ -20,6 +25,28 @@ async def test_paywall_command(mock_interaction: mock.Mock) -> None:
     mock_interaction.response.send_message.assert_awaited_once_with(
         StringContaining(link)
     )
+
+
+@pytest.mark.asyncio
+async def test_paywall_command_strips_params(mock_interaction: mock.Mock) -> None:
+    params = "?param1=foo&param2=bar"
+    link = f"https://foo.com/poop{params}"
+
+    await commands.paywall.callback(mock_interaction, link)
+    mock_interaction.response.send_message.assert_awaited_once_with(
+        StringNotContaining(params)
+    )
+
+
+@pytest.mark.asyncio
+async def test_paywall_command_rejects_invalid_link(
+    mock_interaction: mock.Mock,
+) -> None:
+    link = f"ftp://foo.com/poop"
+
+    with pytest.raises(exception.MemebotUserError):
+        await commands.paywall.callback(mock_interaction, link)
+    mock_interaction.response.send_message.assert_not_awaited()
 
 
 @pytest.mark.asyncio
@@ -37,6 +64,20 @@ async def test_paywall_context_menu_succeeds_with_embed(
 
 
 @pytest.mark.asyncio
+async def test_paywall_context_menu_strips_params_from_embed(
+    mock_interaction: mock.Mock, mock_message: mock.Mock
+) -> None:
+    params = "?param1=foo&param2=bar"
+    link = f"https://foo.com/poop{params}"
+    mock_message.embeds = [mock.Mock(url=link)]
+
+    await commands.paywall_context_menu.callback(mock_interaction, mock_message)
+    mock_interaction.response.send_message.assert_awaited_once_with(
+        StringNotContaining(params)
+    )
+
+
+@pytest.mark.asyncio
 async def test_paywall_context_menu_succeeds_with_content(
     mock_interaction: mock.Mock, mock_message: mock.Mock
 ) -> None:
@@ -47,6 +88,20 @@ async def test_paywall_context_menu_succeeds_with_content(
     await commands.paywall_context_menu.callback(mock_interaction, mock_message)
     mock_interaction.response.send_message.assert_awaited_once_with(
         StringContaining(link)
+    )
+
+
+@pytest.mark.asyncio
+async def test_paywall_context_menu_strips_params_from_content(
+    mock_interaction: mock.Mock, mock_message: mock.Mock
+) -> None:
+    params = "?param1=foo&param2=bar"
+    link = f"https://foo.com/poop{params}"
+    mock_message.content = f"Hey gamers, check this out {link}"
+
+    await commands.paywall_context_menu.callback(mock_interaction, mock_message)
+    mock_interaction.response.send_message.assert_awaited_once_with(
+        StringNotContaining(params)
     )
 
 
