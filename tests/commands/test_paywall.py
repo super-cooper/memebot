4d09f203d210ae399fd0a8193b7256e7cd98e8b2
Nick Hawke <n.hawke96@gmail.com>
2025-06-09 20:41:26 -0400

    fix formatting in test file

--- tests/commands/test_paywall.py
+++ tests/commands/test_paywall.py
@@ -6,9 +6,10 @@ import pytest
 from memebot import commands
 from memebot.lib import exception
 
+
 class StringContaining(str):
     def __eq__(self, other):
-            return self in other
+        return self in other
 
 
 @pytest.mark.asyncio
@@ -16,30 +17,44 @@ async def test_paywall_command(mock_interaction: mock.Mock) -> None:
     link = "https://foo.com/poop"
 
     await commands.paywall.callback(mock_interaction, link)
-    mock_interaction.response.send_message.assert_awaited_once_with(StringContaining(link))
+    mock_interaction.response.send_message.assert_awaited_once_with(
+        StringContaining(link)
+    )
+
 
 @pytest.mark.asyncio
-async def test_paywall_context_menu_succeeds_with_embed(mock_interaction: mock.Mock, mock_message:mock.Mock) -> None:
+async def test_paywall_context_menu_succeeds_with_embed(
+    mock_interaction: mock.Mock, mock_message: mock.Mock
+) -> None:
     link = "https://foo.com/poop"
     bad_link = "http://bar.com/fart"
     mock_message.embeds = [mock.Mock(url=u) for u in (link, bad_link)]
 
     await commands.paywall_context_menu.callback(mock_interaction, mock_message)
-    mock_interaction.response.send_message.assert_awaited_once_with(StringContaining(link))
+    mock_interaction.response.send_message.assert_awaited_once_with(
+        StringContaining(link)
+    )
+
 
 @pytest.mark.asyncio
-async def test_paywall_context_menu_succeeds_with_content(mock_interaction: mock.Mock, mock_message:mock.Mock) -> None:
+async def test_paywall_context_menu_succeeds_with_content(
+    mock_interaction: mock.Mock, mock_message: mock.Mock
+) -> None:
     link = "https://foo.com/poop"
     bad_link = "http://bar.com/fart"
     mock_message.content = f"Hey gamers, check this out {link} {bad_link}"
 
     await commands.paywall_context_menu.callback(mock_interaction, mock_message)
-    mock_interaction.response.send_message.assert_awaited_once_with(StringContaining(link))
+    mock_interaction.response.send_message.assert_awaited_once_with(
+        StringContaining(link)
+    )
+
 
 @pytest.mark.asyncio
-async def test_paywall_context_menu_fails_with_no_link(mock_interaction: mock.Mock, mock_message:mock.Mock) -> None:
+async def test_paywall_context_menu_fails_with_no_link(
+    mock_interaction: mock.Mock, mock_message: mock.Mock
+) -> None:
     mock_message.content = ""
     with pytest.raises(exception.MemebotUserError):
         await commands.paywall_context_menu.callback(mock_interaction, mock_message)
     mock_interaction.response.send_message.assert_not_awaited()
-
