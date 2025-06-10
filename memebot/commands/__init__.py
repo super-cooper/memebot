8b26d592e0a57449c376a2a4a4a86679898168ac
Nick Hawke <n.hawke96@gmail.com>
2025-06-10 19:37:41 -0400

    Improve command registration

--- memebot/commands/__init__.py
+++ memebot/commands/__init__.py
@@ -1,3 +1,12 @@
+import discord.ext.commands
+
 from .hello import hello
 from .paywall import paywall, paywall_context_menu
 from .role import role
+
+
+def register_commands(bot: discord.ext.commands.Bot):
+    bot.tree.add_command(hello)
+    bot.tree.add_command(role)
+    bot.tree.add_command(paywall)
+    bot.tree.add_command(paywall_context_menu)
